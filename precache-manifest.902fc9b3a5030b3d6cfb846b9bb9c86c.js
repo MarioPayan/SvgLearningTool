40f2da4533f2f43a96982a8f5f9c1eb8244689e2
Mario Payan <mariopayan@alertlogic.com>
2019-10-21 10:11:08 -0500

    Updates

---
+++ precache-manifest.902fc9b3a5030b3d6cfb846b9bb9c86c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b869956bcdf8f217b71b3ad951186f",
+    "url": "/MarioPayan/SvgLearningTool/index.html"
+  },
+  {
+    "revision": "6e0533aa23ada6282b09",
+    "url": "/MarioPayan/SvgLearningTool/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ef57fe44b6df52567c7e",
+    "url": "/MarioPayan/SvgLearningTool/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "6e0533aa23ada6282b09",
+    "url": "/MarioPayan/SvgLearningTool/static/js/2.5bee98d1.chunk.js"
+  },
+  {
+    "revision": "ef57fe44b6df52567c7e",
+    "url": "/MarioPayan/SvgLearningTool/static/js/main.f66017f6.chunk.js"
+  },
+  {
+    "revision": "63bbec5e01c706fe3545",
+    "url": "/MarioPayan/SvgLearningTool/static/js/runtime-main.9cd4d6dc.js"
+  }
+]);
